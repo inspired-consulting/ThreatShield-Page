f5fe92fed1b66698dd6ff91558f49191ea1cdde0
“Sophie <sophie.rauhut@inspired.consulting>
2023-11-27 16:30:46 +0100

    fixing text color in footer.hbs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,html}"],
+  content: ["./index.html", "./src/**/*.{js,html,hbs}"],
   theme: {
     extend: {
       colors: {
